91a7b313ad31c2641d03b67e879cb31ce77a3520
oomcore <klwhite@outlook.com>
2014-08-24 23:06:58 +0100

    Programming Assignment 2 mods for matrix inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions that can be used to invert an invertible matrix and cache the result
+## cacheSolve performs inversion and stores result in list data structure previously
+## created by makeCacheMatrix
 
-## Write a short comment describing this function
-
+## creates a list of 4 functions to set and get matrix, and set and get matrix inverse
+## only storage and retrieval, no calcs
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  setM <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  getM <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(setM = setM, getM = getM,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## solves matrix and returns inverse after storing, or returns cached value if it exists
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return cached matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  ## nothing in cache - so calculate and return inverse of 'x'
+  matr <- x$getM()
+  inv <- solve(matr, ...)
+  x$setInverse(inv)
+  inv
 }
